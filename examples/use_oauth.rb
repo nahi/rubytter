f60d23539476e5ec029dae44fac98eea918d19b6
Hiroshi Nakamura <nahi@ruby-lang.org>
2010-03-25 23:31:16 +0900

    removed 'sample' dir.
    wrongly created in the previous commit. merged to 'example/use_oauth.rb'.

--- examples/use_oauth.rb
+++ examples/use_oauth.rb
@@ -1,24 +1,39 @@
 $:.unshift File.join(File.dirname(__FILE__), '../lib')
-require 'rubygems'
+
 require 'rubytter'
 
-key = ""
-secret = ""
+# OAuth example
 
-oauth = Rubytter::OAuth.new(key, secret)
+# create your application at http://twitter.com/apps/new to retrieve consumer token.
+consumer_key = 'key'
+consumer_secret = 'secret'
 
-request_token = oauth.get_request_token
-system('open', request_token.authorize_url) || puts("Access here: #{request_token.authorize_url}\nand...")
+oob_authorize_url = 'https://twitter.com/oauth/authorize'
 
-print "Enter PIN: "
-pin = gets.strip
+# get request token
+consumer = Rubytter::OAuth.new(consumer_key, consumer_secret)
+request_token = consumer.get_request_token
+token = request_token.token
+secret = request_token.secret
 
-access_token = request_token.get_access_token(
-  :oauth_token => request_token.token,
-  :oauth_verifier => pin
-)
+# let your authorize request token. PIN code (verifier) required
+puts "Go here and do confirm: #{oob_authorize_url}?oauth_token=#{token}"
+puts "Type oauth_verifier/PIN (if given) and hit [enter] to go"
+verifier = gets.chomp
 
-client = OAuthRubytter.new(access_token)
-client.friends_timeline.each do |status|
-  puts "#{status.user.screen_name}: #{status.text}"
-end
+# get access token
+access_token = consumer.get_access_token(token, secret, verifier)
+token = access_token.token
+secret = access_token.secret
+
+# initialize OAuthRubytter
+rubytter = OAuthRubytter.new(:token => token, :secret => secret, :consumer => {:key => consumer_key, :secret => consumer_secret})
+
+# for wiredump debugging
+# rubytter.connection.client.debug_dev = STDERR
+
+# sample API usage.
+p rubytter.user_timeline(1).class
+p rubytter.replies.class
+p rubytter.search('ruby openssl')
+p rubytter.search_user('nahi')
